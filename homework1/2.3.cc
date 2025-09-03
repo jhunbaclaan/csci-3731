4ead082db99c5e9361bef4d19d3cbbf946860e31
june <54416373+jhunbaclaan@users.noreply.github.com>
2025-09-03 12:45:07 -1000

    Update 2.3.cc
    see inline comments
    tldr redid method placement for the sake of readability

--- homework1/2.3.cc
+++ homework1/2.3.cc
@@ -6,18 +6,13 @@ mandelbrot set program that uses static values a & b iteratively with scaling va
 #include <cstdio>
 
 // simplifying the mandelbrot equations by making functions
-// interestingly functions have to be put above the main method (unlike java where they can js be anywhere outside)
+// interestingly methods have to be put above main (unlike java where they can js be anywhere outside)
 // it's a strange adjustment
-int xprime(int a, int x, int y)
-{
-    int result = (x*x)- (y*y)+a;
-    return result;
-}
-int yprime(int b, int x, int y)
-{
-    int result = 2*(x*y) + b;
-    return result;
-}
+
+// 08/03: section 3.2.2 of the book actually solves this!! you have to declare at the top but it doesnt have to be the full method
+// this makes it a bit easier...
+int xprime(int a, int x, int y);
+int yprime(int b, int x, int y);
 
 // main
 int main(int argc, char** agv)
@@ -28,7 +23,6 @@ int main(int argc, char** agv)
     // x & y start as 0 as given by the problem
     int x = 0;
     int y = 0;
-
     /*
     iteration is interesting. i dont know how long we're supposed to iterate for
     so maybe i'll make a user system that continues to iterate if the user confirms?
@@ -40,4 +34,15 @@ int main(int argc, char** agv)
     y = yprime(b,x,y);
     printf("iteration 2: a = %d, b = %d, x = %d, y = %d\n", a,b,x,y);
     
+}
+// defining the methods that were declared above
+int xprime(int a, int x, int y)
+{
+    int result = (x*x)- (y*y)+a;
+    return result;
+}
+int yprime(int b, int x, int y)
+{
+    int result = 2*(x*y) + b;
+    return result;
 }
