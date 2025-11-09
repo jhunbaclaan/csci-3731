767b92185c6c6619c61ccd122d4c0c1baddf644b
june <54416373+jhunbaclaan@users.noreply.github.com>
2025-11-09 00:31:37 -1000

    short answer questions done
    short answers done and im gonna try to finish 9.3 before 12pm but no guarantees and it might be turned in later tmrw

--- hw7/8.1.cc
+++ hw7/8.1.cc
@@ -2,6 +2,6 @@
 int main(int args, char** agv)
 {
     imageMaker img("test.ppm");
-    img+=(-80);
+    img+=(-80); // negative values darkens the image, positive values brighten it; -80 consumes my poor professor's dog with darkness....
     img.makePPM("copy.ppm");
 }
